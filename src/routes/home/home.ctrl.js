c61faa24183b16ef4def267f2551f1b8f65d4e01
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-11-21 18:42:43 +0900

    초기 폴더 시스템

---
+++ src/routes/home/home.ctrl.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const output = {
+    home : (req, res) => {
+        res.render('home/index')
+    }
+}
+
+
+//post방식으로 보낼 코드
+const process = {
+
+};
+
+
+module.exports = {
+    output,
+    // process
+}
---
+++ src/routes/home/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+const express = require('express');
+const router = express.Router();
+
+const ctrl = require('./home.ctrl')
+
+// ctrl 폴더에서 output이나 process 꺼내기
+router.get('/', ctrl.output.home)
+
+module.exports= router;
