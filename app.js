c25dfab3c6378abac6abc61488c53a7f583ce8e5
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-11-27 12:36:05 +0900

    commit before pull

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// app.js
-
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const server = require('http').createServer(app);
-const path = require("path");
-const socketIO = require('socket.io');
-const io = socketIO(server);
 
 const home = require('./src/routes/home');
 
@@ -14,36 +8,11 @@ const home = require('./src/routes/home');
 app.set('views', "./src/views");
 app.set('view engine', 'ejs');
 // 미들웨어
-app.use(express.static(`${__dirname}/src`))
-// const socketPath = '/path/to/socket.io-client';
-// app.use(socketPath, express.static(path.join(__dirname, 'node_modules', 'socket.io-client', 'dist')));
+app.use(express.static(`${__dirname}/src/public`))
 app.use(bodyParser.json());
-// 인코딩 위함
-app.use(bodyParser.urlencoded({extended: true}));
+//인코딩 위함
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use('/', home);
 
-
-io.on('connection', (socket)=>{
-    console.log('연결');
-    socket.on('chatting', (data)=>{
-        const {            
-            id,
-            StudentID, 
-            name, 
-            startTime, 
-            endTime} = data;
-        io.emit('chatting', {
-            id,
-            StudentID, 
-            name, 
-            startTime, 
-            endTime
-        })
-    })
-})
-
-// io.path('/socket.io');
-
-
-module.exports = server;
+module.exports = app;
