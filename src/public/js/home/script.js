3b24decda89d6a78a14b024ff439146b1ad2e96b
Lee-jeong-wook <126264218+Lee-jeong-wook@users.noreply.github.com>
2023-12-10 16:43:33 +0900

    수정 입력칸 생성

--- src/public/js/home/script.js
+++ src/public/js/home/script.js
@@ -3,41 +3,12 @@ const subAddImg = document.querySelector('.sub-add-img a');
 let marginValues = ['0 0 10px 260px', '0 0 10px 80px', '0 0 10px 150px', '0 0 10px 160px'];
 let currentIndex = 0;
 
-// subAddImg.addEventListener('click', () => {
-//     const container = document.createElement('div');
-//     container.classList.add('poriv');
-//     container.style.display = 'inline-flex';
-//     container.style.margin = marginValues[currentIndex];
-
-//     const newInput = document.createElement('input');
-//     newInput.type = 'text';
-
-//     const deleteButton = document.createElement('button');
-//     deleteButton.textContent = 'x';
-//     deleteButton.style.width = '30px';
-//     deleteButton.style.height = '30px';
-//     deleteButton.style.fontSize = '18px';
-//     deleteButton.style.backgroundColor = 'red';
-//     deleteButton.style.color = 'white';
-//     deleteButton.style.border = 'none';
-//     deleteButton.style.cursor = 'pointer';
-
-//     deleteButton.addEventListener('click', () => {
-//         container.remove();
-//     });
-
-//     container.appendChild(newInput);
-//     container.appendChild(deleteButton);
-
-//     document.body.appendChild(container);
-
-//     currentIndex = (currentIndex + 1) % marginValues.length; 
-// });
-//팝업//
 const header = document.querySelector('.head');
 const h1 = header.querySelector('h1');
 const h2 = header.querySelector('h2');
 const last = document.getElementsByClassName('last-img')[0];
+
+const changeImg = document.querySelector(".change-img");
 
 const closeButton = document.createElement('a');
 closeButton.href = '#';
@@ -45,6 +16,17 @@ closeButton.innerText = 'x';
 closeButton.classList.add('bottn');
 
 last.appendChild(closeButton);
+
+let isChange = false;
+changeImg.addEventListener("click", () => {
+    console.log(isChange);
+    if(isChange){
+        changeImg.parentElement.classList.remove("active");         
+    }else{
+        changeImg.parentElement.classList.add("active");
+    }
+    isChange = !isChange;
+})
 
 h2.onclick = function (event) {
     event.stopPropagation();
